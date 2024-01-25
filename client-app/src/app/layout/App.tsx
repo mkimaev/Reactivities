beb8abde21a34e8b7f1063ec47e2a83d18b83878
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-25 19:01:10 +0200

    sec_5: reorganized App.tsx + added new interface Activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './styles.css'
 import axios from 'axios';
 import { Header, List, ListItem } from 'semantic-ui-react';
+import { Activity } from './models/activity';
 
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         setActivities(response.data)
       })
@@ -18,7 +19,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivities' />
       <List>
-        {activities.map((activity: any) => (
+        {activities.map(activity => (
           <ListItem key={activity.id}>
             {activity.title}
           </ListItem>
---
+++ client-app/src/app/layout/models/activity.ts
@@ -0,0 +1,19 @@
+export interface Activity {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    city: string
+    venue: string
+  }
+
+ /*  export type Activity =  {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    city: string
+    venue: string
+  } */
