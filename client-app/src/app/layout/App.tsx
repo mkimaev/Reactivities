465c50b04e509a524daa7a41933d8276a00e0103
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-25 19:49:27 +0200

    5: added dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import './styles.css'
 import axios from 'axios';
-import { Header, List, ListItem } from 'semantic-ui-react';
+import { Container, List, ListItem } from 'semantic-ui-react';
 import { Activity } from './models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 
 function App() {
@@ -16,16 +18,16 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-      <List>
-        {activities.map(activity => (
-          <ListItem key={activity.id}>
-            {activity.title}
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar />
+
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </Fragment>
+
+
+
   )
 }
 
