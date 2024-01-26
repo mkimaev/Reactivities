ad7eb7478097d9c1b631726b81f85686f0f7c2b6
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-26 18:23:30 +0200

    6: Posting data to the server

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity | undefined;
     closeFrom: () => void;
     createOrEdit: (activity : Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selected, closeFrom, createOrEdit}: Props){
+export default function ActivityForm({activity: selected, closeFrom, createOrEdit, submitting}: Props){
 
     const initialState = selected ?? {
         id: '',
@@ -40,7 +41,7 @@ export default function ActivityForm({activity: selected, closeFrom, createOrEdi
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button floated="right" positive type="submit" content="Submit" />
+                <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
                 <Button onClick={closeFrom} floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
