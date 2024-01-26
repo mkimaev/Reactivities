8ee02b01dfecab6cc48c7f49bf221d8075b71a09
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-26 13:15:45 +0200

    disable Strict mode for a while

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layout/App.tsx'
 import './app/layout/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+     <App />
+  // </React.StrictMode>,
 )
