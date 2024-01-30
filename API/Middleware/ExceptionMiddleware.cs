42c155ce2fb745c6dd99c86984e7424b71f9a735
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-30 17:21:20 +0200

    10: added Excep middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using System.Text.Json;
+using Application.Core;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this.logger = logger;
+            this.next = next;
+            this.env = env;
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var responseEx = env.IsDevelopment()
+                ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                : new AppException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(responseEx, options);
+
+                await context.Response.WriteAsync(json);
+                
+            }
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using Application;
 using Application.Activities;
 using Application.Core;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,8 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
