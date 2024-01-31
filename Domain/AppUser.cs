810f5990e5a1ec626f588e559350c68a488491a0
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-01-31 19:01:19 +0200

    12: added EF core package to Domain + AppUser entity

---
+++ Domain/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+
+
+namespace Domain
+{
+	public class AppUser: IdentityUser
+	{
+		public string DisplayName { get; set; }
+        public string Bio { get; set; }
+    }
+
+}
