b2e2442877fdcf885a382299e2fe07a5e3f4fa69
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-02-01 16:46:18 +0200

    12: added Register method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,10 +4,11 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
-	[AllowAnonymous]
 	[ApiController]
 	[Route("api/[controller]")]
 	public class AccountController : ControllerBase
@@ -21,6 +22,7 @@ namespace API.Controllers
 			this.tokenService = tokenService;
 		}
 
+		[AllowAnonymous]
 		[HttpPost("login")]
 		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
@@ -46,5 +48,50 @@ namespace API.Controllers
 
 			return Unauthorized();
 		}
-    }
+
+		[AllowAnonymous]
+		[HttpPost("register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+		{
+			if (await userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
+			{
+				return BadRequest("Username is already taken");
+			}
+
+			var user = new AppUser
+			{
+				DisplayName = registerDto.DisplayName,
+				Email = registerDto.Email,
+				UserName = registerDto.Username
+			};
+
+			var result = await userManager.CreateAsync(user, registerDto.Password);
+
+			if (result.Succeeded)
+			{
+				return CreateUserObject(user);
+			}
+
+			return BadRequest(result.Errors);
+		}
+
+		
+		[HttpGet]
+		public async Task<ActionResult<UserDto>> GetCurrrentUser()
+		{
+			var user = await userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+			return this.CreateUserObject(user);
+		}
+
+		private UserDto CreateUserObject(AppUser user)
+		{
+			return new UserDto
+			{
+				DisplayName = user.DisplayName,
+				Image = null,
+				Token = tokenService.CreateToken(user),
+				Username = user.UserName
+			};
+		}
+	}
 }
