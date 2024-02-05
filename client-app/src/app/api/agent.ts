ae2b0716aa9f00554df9daf63e13c17f0456066a
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-02-05 20:46:04 +0200

    15: added attend method on ui

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -80,7 +80,8 @@ const Activities = {
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => requests.post<void>('/activities', activity),
     update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => requests.delete<Activity>(`/activities/${id}`)
+    delete: (id: string) => requests.delete<Activity>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
