0aa3c18cafb7aa70d865c04d11ef3eb1d61023c3
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-02-06 17:42:38 +0200

    17: added Profile pages + activated

--- client-app/src/features/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/details/ActivityDetailedHeader.tsx
@@ -43,8 +43,7 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
                                 />
                                 <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                                 <p>
-                                    Hosted by
-                                    <strong>
+                                    Hosted by <strong>
                                         <Link to={`/profiles/${activity.host?.username}`} >
                                             {activity.host?.displayName}
                                         </Link>
