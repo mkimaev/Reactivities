daef365fa48ab632195e152c70684c0917dfa934
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-02-06 17:58:28 +0200

    17: added new item in store:  ProfileStore

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -3,6 +3,7 @@ import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
 import UserStore from "./userStore";
 import ModalStore from "./modalStore";
+import ProfileStore from "./profileStore";
 
 
 interface Store{
@@ -10,13 +11,15 @@ interface Store{
     commonStore: CommonStore;
     userStore: UserStore;
     modalStore: ModalStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
     userStore: new UserStore(),
-    modalStore: new ModalStore()
+    modalStore: new ModalStore(),
+    profileStore: new ProfileStore()
 }
 
 export const StoreContext = createContext(store);
