a1a3e6219f271e03dfe4beeda7a230a2a7c6db88
maksym.kimaev <maksym.kimaev@nixsolutions.com>
2024-02-07 17:48:12 +0200

    19: added BL for Comments in Application lvl

---
+++ Application/Comments/Create.cs
@@ -0,0 +1,73 @@
+﻿
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comments
+{
+	public class Create
+	{
+		public class Command : IRequest<Result<CommentDto>>
+		{
+			public string Body { get; set; }
+			public Guid ActivityId { get; set; }
+		}
+
+		public class CommandValidator : AbstractValidator<Command>
+		{
+			public CommandValidator()
+			{
+				RuleFor(item => item.Body).NotEmpty();
+			}
+		}
+
+		public class Handler : IRequestHandler<Command, Result<CommentDto>>
+		{
+			private readonly DataContext dataContext;
+			private readonly IMapper mapper;
+			private readonly IUserAccessor userAccessor;
+
+			public Handler(DataContext dataContext, IMapper mapper, IUserAccessor userAccessor)
+			{
+				this.dataContext = dataContext;
+				this.mapper = mapper;
+				this.userAccessor = userAccessor;
+			}
+
+			public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+			{
+				var activity = await dataContext.Activities.FindAsync(request.ActivityId);
+
+				if (activity == null) { return null; }
+
+				var user = await dataContext.Users
+					.Include(p => p.Photos)
+					.SingleOrDefaultAsync(x => x.UserName == userAccessor.GetUsername());
+
+				var comment = new Comment
+				{
+					Author = user,
+					Activity = activity,
+					Body = request.Body
+				};
+
+				activity.Comments.Add(comment);
+
+				var success = await dataContext.SaveChangesAsync() > 0;
+
+				if (success)
+				{
+					return Result<CommentDto>.Success(mapper.Map<CommentDto>(comment));
+				}
+
+				return Result<CommentDto>.Failure("Failed to add commment");
+
+			}
+		}
+	}
+}
---
+++ Application/Comments/List.cs
@@ -0,0 +1,41 @@
+﻿
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+
+namespace Application.Comments
+{
+	public class List
+	{
+		public class Query : IRequest<Result<List<CommentDto>>> 
+		{
+            public Guid ActivityId { get; set; }
+        }
+
+		public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+		{
+			private readonly DataContext dataContext;
+			private readonly IMapper mapper;
+
+			public Handler(DataContext dataContext, IMapper mapper)
+			{
+				this.dataContext = dataContext;
+				this.mapper = mapper;
+			}
+			public async Task<Result<List<CommentDto>>> Handle(Query request, CancellationToken cancellationToken)
+			{
+				var comments = await dataContext.Comments
+					.Where(x => x.Activity.Id == request.ActivityId)
+					.OrderBy(x => x.CreatedAt)
+					.ProjectTo<CommentDto>(mapper.ConfigurationProvider)
+					.ToListAsync(cancellationToken);
+
+				return Result<List<CommentDto>>.Success(comments);
+			}
+		}
+	}
+}
